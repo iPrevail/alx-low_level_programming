500d612d072136602d0db65e6898c322558c5fc6
iPrevail <prevailerkk@gmail.com>
2023-06-22 03:24:45 -0700

    print jack bauers time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of jackB
+ *
+ * Returns: nothing
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+
+	while (hour < 24)
+	{
+		int minute = 0;
+
+		while (minute < 60)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			minute++;
+		}
+		_putchar('\n');
+		hour++;
+	}
+}

3775a3a3d8faac7ebcd9e06b57229270a87f12c7
iPrevail <prevailerkk@gmail.com>
2023-06-23 02:44:29 -0700

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - to check for a digit 0 through 9
+ * @a: first number to multiply
+ * @b: second number to multiply
+ * Return: 1 if digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}

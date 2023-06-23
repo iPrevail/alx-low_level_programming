52ba58d7e69f4ac524ed0cec879445d3ba2ca432
iPrevail <prevailerkk@gmail.com>
2023-06-23 03:28:20 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -22,8 +22,8 @@ void more_numbers(void)
 			}
 			else
 			{
-			_putchar('0' + j / 10);
-			_putchar('0' + j % 10);
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
 			}
 		}
 		_putchar('\n')

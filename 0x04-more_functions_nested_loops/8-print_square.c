ab324f9e1fbffe3369b38a44e500524b76c65d03
iPrevail <prevailerkk@gmail.com>
2023-06-23 15:02:40 -0700

    modified

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,19 +7,19 @@
  */
 void print_square(int size)
 {
-	int i = 0;
-
-	if (!(size <= 0))
+	if (size <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i = 0;
+
 		while (i < size)
 		{
 			_putchar('#');
 			i++;
 		}
-		_putchar('\n');
-	}
-	else
-	{
 		_putchar('\n');
 	}
 }

3c178ff7a663cc23b7366b9318e5f4c18c08cf78
iPrevail <prevailerkk@gmail.com>
2023-06-23 16:30:10 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: the size
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i = 1;
+
+		while (i <= n)
+		{
+			int j = 1;
+
+			while (j < 1)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+}

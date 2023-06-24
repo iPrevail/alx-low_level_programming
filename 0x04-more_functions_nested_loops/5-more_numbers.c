51fb0bfaff25f1685e819bcb4ac9c486e9533f6e
iPrevail <prevailerkk@gmail.com>
2023-06-23 20:54:14 -0700

    modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,15 +15,11 @@ void more_numbers(void)
 
 		while (j < 15)
 		{
-			if (j < 10)
-			{
-				_putchar('0' + j);
-			}
-			else
+			if (j > 9)
 			{
 				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
 			}
+			_putchar('0' + j % 10);
 			j++;
 		}
 		_putchar('\n');

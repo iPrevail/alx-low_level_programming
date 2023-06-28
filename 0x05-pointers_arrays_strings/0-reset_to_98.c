0ee341720bd6e7050eb4390c5ea3e24248cf9242
iPrevail <prevailerkk@gmail.com>
2023-06-28 05:27:56 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function to reset the value to 98
+ * @n: Pointer to int
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#include <unistd>
+
+int _putchar(char c);
+void reset_to_98(int *n);

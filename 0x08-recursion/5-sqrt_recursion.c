fa88b8657ef04858cb15c6734ece44ba3e55babf
iPrevail <prevailerkk@gmail.com>
2023-11-16 05:50:17 -0800

    fixed it

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -28,13 +28,12 @@ int sqrt_check(int n, int x)
 int _sqrt_recursion(int n)
 {
 	if (n == 1)
-	{
 		return (1);
-	}
 	else if (n == 0)
-	{
 		return (0);
-	}
+	else if (n < 0)
+		return (-1);
 	else
 		return (sqrt_check(n, 2));
+}
 

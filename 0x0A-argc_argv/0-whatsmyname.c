82cd01c60398575b306827eda094366b8f0899bb
iPrevail <prevailerkk@gmail.com>
2023-11-20 07:40:48 -0800

    the function to print the name of the program file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - to print the name of the program file
+ * @argc: the number of string arguments
+ * @argv: the array of pointers to the string arguments
+ *
+ * Rturn: Always 0, if success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

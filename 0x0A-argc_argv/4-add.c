f1993cca62b7f4c5586d712a1542c9d01f95499d
iPrevail <prevailerkk@gmail.com>
2023-11-21 05:41:50 -0800

    print the sum of its CL arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the sum of two CL arguments
+ * @argc: number of arguments
+ * @argv: array of string arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) > 0)
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+

e76cf862802cc8b5dbada8df9414269be1df93c8
iPrevail <prevailerkk@gmail.com>
2024-03-08 15:05:05 -0800

    a function that prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in the reverse order
+ * @s: the string from the calling function
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+	_putchar('\n');
+}

38e7d472035b502fb6521ddeb955036c7eb8e8a6
iPrevail <prevailerkk@gmail.com>
2024-03-08 15:18:23 -0800

    function reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,4 @@
 #include "main.h"
-
-/**
- * rev_string - reverses the order of characters in a
- * string in the calling function.
- * @s: the string to reverse
- */
-
-void rev_string(char *s)
-{
-	int l, j, temp;
-
-	for (l = 0; s[l] != '\0'; l++)
-		;
-	for (j = 0; j <= l / 2; j++)
-	{
-		temp = s[j];
-		s[j] = s[l - j - 1];
-		s[l - j - 1] = temp;
-	}
-}
-#include "main.h"
 /**
 * rev_string - function reverse a string
 *
@@ -42,25 +21,4 @@ rev = s[i];
 s[i] = s[counter];
 s[counter] = rev;
 }
-}
-#include "main.h"
-
-/**
- * rev_string - reverses the order of characters in a
- * string in the calling function.
- * @s: the string to reverse
- */
-
-void rev_string(char *s)
-{
-	int l, j, temp;
-
-	for (l = 0; s[l] != '\0'; l++)
-		;
-	for (j = 0; j <= l / 2; j++)
-	{
-		temp = s[j];
-		s[j] = s[l - j - 1];
-		s[l - j - 1] = temp;
-	}
 }

dec933904edaa8ac6c66e593df22cb0d7cafd0c6
iPrevail <prevailerkk@gmail.com>
2024-03-08 15:54:20 -0800

    a function that prints n elements of an array of integers, followed by a new line

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,25 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* print_array - a function that prints n number
-* of elements of an array
-*
-* @a: spring parameter to be read
-*
-* @n: read parameter of of elements
-*
-* Return: always 0
-*/
+ * print_array - print a certain number of elements from
+ * an array of integers
+ * @a: the integer array to print from
+ * @n: the number of elements to print
+ */
+
 void print_array(int *a, int n)
 {
-int i;
+	int i;
 
-for (i = 0 ; i < (n - 1) ; i++)
-{
-printf("%d, ", a[i]);
-}
-if (i == (n - 1))
-{
-printf("%d", a[n - 1]);
-}
-printf("\n");
+	i = 0;
+
+	if (i >= n)
+		printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
 }
